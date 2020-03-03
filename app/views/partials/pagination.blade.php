510d5dabf5866fd42d76d79353b52e545e112e22
neo <enginesilver@gmail.com>
2020-03-03 08:57:14 +0100

    added custom pagination for tailwind support

--- app/views/partials/pagination.blade.php
+++ app/views/partials/pagination.blade.php
@@ -1,10 +1,13 @@
 @if ($paginator->getLastPage() > 1)
-<div class="text-center w-100">
-    <a href="{{ $paginator->getUrl(1) }}" id="page[1]" class="{{($paginator->getCurrentPage() == 1) ? ' disabled' : '' }}">Previous</a>
-    @for ($i = 1; $i <= $paginator->getLastPage(); $i++)
-        <a href="{{ $paginator->getUrl($i) }}" id="page[{{$i}}]" class="{{($paginator->getCurrentPage() == $i) ? ' active' : '' }}">{{ $i }}</a>
-    @endfor
-    <a href="{{ $paginator->getUrl($paginator->getCurrentPage()+1) }}" id="page[{{$paginator->getCurrentPage()+1}}]" class="{{($paginator->getCurrentPage() == $paginator->getLastPage()) ? ' disabled' : '' }}">Next</a>
-</div>
-<hr>
+    <div class="text-center w-100">
+        <a href="{{ $paginator->getUrl(1) }}" id="page[1]"
+           class="{{($paginator->getCurrentPage() == 1) ? ' disabled' : '' }}">Previous</a>
+        @for ($i = 1; $i <= $paginator->getLastPage(); $i++)
+            <a href="{{ $paginator->getUrl($i) }}" id="page[{{$i}}]"
+               class="{{($paginator->getCurrentPage() == $i) ? ' active' : '' }}">{{ $i }}</a>
+        @endfor
+        <a href="{{ $paginator->getUrl($paginator->getCurrentPage()+1) }}" id="page[{{$paginator->getCurrentPage()+1}}]"
+           class="{{($paginator->getCurrentPage() == $paginator->getLastPage()) ? ' disabled' : '' }}">Next</a>
+    </div>
+    <hr>
 @endif
